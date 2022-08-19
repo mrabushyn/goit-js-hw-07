24d9f30c425c86fc73fda648d3ae3b1879c8b1a1
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-08-19 18:07:19 +0300

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListItem = document.querySelector(".gallery");
+
+const createList = createListItem(galleryItems);
+
+function createListItem(items) {
+    return items
+        .map(
+            ({ preview, original, description }) =>
+                `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`
+        )
+        .join("");
+}
+
+galleryListItem.insertAdjacentHTML("beforeend", createList);
+
+galleryListItem.addEventListener("click", largePicture);
+
+function largePicture(e) {
+    e.preventDefault();
+    if (!e.target.classList.contains("gallery__image")) 
+    return;
+        const showLargePic = basicLightbox.create(`<img src="${e.target.dataset.source}">`)
+        showLargePic.show()
+    
+}
