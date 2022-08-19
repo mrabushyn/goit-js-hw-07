eebfcfd673546648baee5cdb5bdf218e955dd285
mrabushyn <99904851+mrabushyn@users.noreply.github.com>
2022-08-19 20:12:47 +0300

    task - 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const mainGalleryItemsContainer = document.querySelector(".gallery");
+
+const createGallery = galleryItem(galleryItems);
+
+function galleryItem(items) {
+    return items
+        .map(
+            ({ preview, original, description }) =>
+                `<a  href="${original}" class="gallery__item">
+  <img 
+  class="gallery__image" 
+  src="${preview}" 
+  alt=""
+  title="${description}"/>
+</a>`
+        )
+        .join("");
+}
+
+mainGalleryItemsContainer.insertAdjacentHTML("beforeend", createGallery);
+
+mainGalleryItemsContainer.addEventListener("click", createSliderGallery);
+
+function createSliderGallery(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains("gallery__image")) return;
+    var lightbox = new SimpleLightbox(".gallery a", {
+        captionPosition: "bottom",
+        captionDelay: 250,
+    });
+    // lightbox()
+}
